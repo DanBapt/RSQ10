9db79fd586c83f06e54507395aa3609578261896
Paul <paul221122112211@gmail.com>
2023-06-19 18:20:06 +0200

    ajout couleurs

--- app/src/main/java/com/example/rsq/Pump/PumpResultsFragment.java
+++ app/src/main/java/com/example/rsq/Pump/PumpResultsFragment.java
@@ -1,9 +1,16 @@
 package com.example.rsq.Pump;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,29 +41,41 @@ public class PumpResultsFragment extends Fragment {
 
         TextView resultsTextView = root.findViewById(R.id.resultsTextView);
 
+        ForegroundColorSpan colorSpanParticipant = new ForegroundColorSpan(Color.RED);  // Couleur bleue pour le nom du participant.
+        ForegroundColorSpan colorSpanQuestion = new ForegroundColorSpan(Color.BLUE);  // Couleur rouge pour la question.
+        ForegroundColorSpan colorSpanAnswer = new ForegroundColorSpan(Color.BLACK);  // Couleur verte pour la réponse.
+
         // Observe the participant answers in the ViewModel
         quizViewModel.getParticipantAnswers().observe(getViewLifecycleOwner(), new Observer<Map<String, List<String>>>() {
             @Override
             public void onChanged(Map<String, List<String>> participantAnswers) {
-                StringBuilder resultsStringBuilder = new StringBuilder();
+                SpannableStringBuilder resultsStringBuilder = new SpannableStringBuilder();
 
                 for (String participantName : participantAnswers.keySet()) {
-                    resultsStringBuilder.append(participantName);
-                    resultsStringBuilder.append("\n");
+                    // Ajouter le nom du participant.
+                    SpannableString participantSpannable = new SpannableString(participantName + "\n");
+                    participantSpannable.setSpan(new ForegroundColorSpan(Color.RED), 0, participantName.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);  // Couleur rouge pour le nom du participant.
+                    participantSpannable.setSpan(new AbsoluteSizeSpan(20, true), 0, participantName.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);  // Taille 20dp pour le nom du participant.
+                    resultsStringBuilder.append(participantSpannable);
 
                     List<String> answers = participantAnswers.get(participantName);
                     for (int i = 0; i < answers.size(); i++) {
                         // Fetch the question from the list of questions stored in QuizViewModel.
-                        String question_pump = quizViewModel.questions_pump.get(i);
-                        resultsStringBuilder.append(question_pump);
-                        resultsStringBuilder.append("\n- ");
-                        resultsStringBuilder.append(answers.get(i));
-                        resultsStringBuilder.append("\n");
+                        String question = quizViewModel.questions.get(i);
+                        SpannableString questionSpannable = new SpannableString(question + "\n- ");
+                        questionSpannable.setSpan(new ForegroundColorSpan(Color.BLUE), 0, question.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);  // Couleur bleue pour la question.
+                        resultsStringBuilder.append(questionSpannable);
+
+                        // Ajouter la réponse.
+                        String answer = answers.get(i);
+                        SpannableString answerSpannable = new SpannableString(answer + "\n");
+                        answerSpannable.setSpan(new ForegroundColorSpan(Color.BLACK), 0, answer.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);  // Couleur noire pour la réponse.
+                        resultsStringBuilder.append(answerSpannable);
                     }
                     resultsStringBuilder.append("\n");
                 }
 
-                resultsTextView.setText(resultsStringBuilder.toString());
+                resultsTextView.setText(resultsStringBuilder);
             }
         });
 
